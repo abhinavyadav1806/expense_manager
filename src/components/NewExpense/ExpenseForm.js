107df039b96c7291012945b594b850bba8ea1eed
abhinavyadav1806 <abhinav1806yadav@gmail.com>
2023-07-09 21:58:52 +0530

    78: Handle Form submission

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,61 +2,89 @@ import React, {useState} from "react";
 import './ExpenseForm.css';
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
 
-    const [userinput, setUserInput] = useState({
-        enteredTitle : '',
-        enteredAmount : '',
-        enteredDate : ''
-    });
+    // const [userinput, setUserInput] = useState({
+    //     enteredTitle : '',
+    //     enteredAmount : '',
+    //     enteredDate : ''
+    // });
 
     const titleChangeHandler = (event) => {
-        // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userinput,
-            enteredTitle : event.target.value
-        });
+        setEnteredTitle(event.target.value);
+        
+        // setUserInput({
+        //     ...userinput,
+        //     enteredTitle : event.target.value
+        // });
+
+        // Must use this sytax whenever your state updates depends on the previous state
+        // setUserInput( (prevState) => {
+        //     return {...prevState, enteredTitle: event.target.value};
+        // })
     };
 
     const amountChangeHandler = (event) => {
-        // setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userinput,
-            enteredAmount: event.target.value
-        });
+        setEnteredAmount(event.target.value);
+        // setUserInput({
+        //     ...userinput,
+        //     enteredAmount: event.target.value
+        // });
     };
 
     const dateChangeHandler = (event) => {
-        // setEnteredDate(event.target.value);
-        setUserInput({
-            ...userinput,
-            enteredDate: event.target.value
-        });
+        setEnteredDate(event.target.value);
+        // setUserInput({
+        //     ...userinput,
+        //     enteredDate: event.target.value
+        // });
+    };
+
+    const inutChangehandler = (identifier, value) => {
+        if ( identifier === 'title') {
+            setEnteredTitle(value);
+        } else if ( identifier === 'date') {
+            setEnteredDate(value);
+        } else {
+            setEnteredAmount(value);
+        }
+    };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            date: new Date(enteredDate),
+            amount: enteredAmount
+        };
+
+        console.log(expenseData);
     };
 
     return (
-        <form className="new-expense__controls">
-            <div className="new-expense__control">
-                <label>Title</label>
-                <input type="text" onChange={titleChangeHandler} ></input>
-            </div>
+        <form onSubmit={submitHandler}>
+            <div className="new-expense__controls">
+                <div className="new-expense__control">
+                    <label>Title</label>
+                    <input type="text" onChange={ (event) => inutChangehandler('title', event.target.value) } ></input>
+                </div>
 
-            <div className="new-expense__control">
-                <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} ></input>
-            </div>
+                <div className="new-expense__control">
+                    <label>Amount</label>
+                    <input type="number" min="0.01" step="0.01" onChange={ (event) => { inutChangehandler('amount', event.target.value); } } ></input>
+                </div>
 
-            <div className="new-expense__control">
-                <label>Date</label>
-                <input type="date" min="2021-01-01" max="2023-07-31" onChange={dateChangeHandler} ></input>
-            </div>
+                <div className="new-expense__control">
+                    <label>Date</label>
+                    <input type="date" min="2021-01-01" max="2023-07-31" onChange={ (event) => { inutChangehandler('date', event.target.value) } } ></input>
+                </div>
 
-            <div className="new-expense__actions">
-                <button type="submit">
-                    Add Expense
-                </button>
+                <div className="new-expense__actions">
+                    <button type="submit">Add Expense</button>
+                </div>
             </div>
         </form>
     );
