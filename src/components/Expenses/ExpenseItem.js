8e4c4c62785f2c48eb9b55d9a0e1bc5b898b5d16
abhinavyadav1806 <abhinav1806yadav@gmail.com>
2023-07-18 00:41:09 +0530

    90

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -22,13 +22,15 @@ const ExpenseItem = (props) => {
     //     <div className="expense-item__price">${amount}</div>
     // </div>
 
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div className='expense-item__price'>${props.amount}</div>
-      </div>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
 }
 
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,6 +4,7 @@ import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import "./Expenses.css";
 import ExpensesFilter from "./ExpensesFilter";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2023");
@@ -16,19 +17,19 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expenseContent = <p>No expenses found in year {filteredYear}.</p>;
-  if ( filteredExpense.length > 0 ) {
-    expenseContent = filteredExpense.length > 0 && (
-      filteredExpense.map((expense) => (
-        <ExpenseItem
-          key={expense.id}
-          title={expense.title}
-          amount={expense.amount}
-          date={expense.date}
-        />
-      ))
-    )
-  }
+  // let expenseContent = <p>No expenses found in year {filteredYear}.</p>;
+  // if ( filteredExpense.length > 0 ) {
+  //   expenseContent = filteredExpense.length > 0 && (
+  //     filteredExpense.map((expense) => (
+  //       <ExpenseItem
+  //         key={expense.id}
+  //         title={expense.title}
+  //         amount={expense.amount}
+  //         date={expense.date}
+  //       />
+  //     ))
+  //   )
+  // }
 
   return (
     <div>
@@ -50,7 +51,8 @@ const Expenses = (props) => {
           ))
         )} */}
 
-        {expenseContent}
+        {/* {expenseContent} */}
+        <ExpensesList item={filteredExpense}/>
       </Card>
     </div>
   );
